288a0f53ee7e2d5abc29552826e95d73f23484b3
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-01-23 18:17:53 +0400

    Update While5.java

--- While5.java
+++ While5.java
@@ -1,10 +1,10 @@
-public class While5{
-	public static void main(String[] arg){
-		int i=0;
-		while (i<=90){
-			i=i+20;
-			System.out.println(i);
-		}
-		
-	}
+public class While5 {
+  public static void main(String[] arg) {
+    int i = 0;
+    while (i <= 90) {
+      i = i + 20;
+      System.out.println(i);
+    }
+
+  }
 }
